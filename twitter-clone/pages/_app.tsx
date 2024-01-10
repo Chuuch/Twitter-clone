abcd6312a015067d9514025aff59b094d25beee7
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-10 12:52:44 -0800

    added Login & Register models to app component

--- twitter-clone/pages/_app.tsx
+++ twitter-clone/pages/_app.tsx
@@ -1,10 +1,14 @@
 import Layout from '@/components/Layout/Layout';
+import LoginModel from '@/components/Models/LoginModel/LoginModel';
+import RegisterModel from '@/components/Models/RegisterModel/RegisterModel';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<RegisterModel />
+			<LoginModel />
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
