06fe827120d143b749a42a71302387918383d3c6
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-10 23:17:45 -0800

    Followbar mapped users

--- twitter-clone/components/Layout/FollowBar/FollowBar.tsx
+++ twitter-clone/components/Layout/FollowBar/FollowBar.tsx
@@ -1,11 +1,13 @@
+import Avatar from "@/components/Avatar/Avatar";
+import useUsers from "@/hooks/useUsers"
 
-interface FollowBarProps {
+const FollowBar = () => {
+  const { data: users = [] } = useUsers();
 
-}
+  if (users.length === 0) {
+    return null;
+  }
 
-const FollowBar: React.FC<FollowBarProps> = ({
-
-}) => {
   return (
     <div className="px-6 py-4 hidden lg:block">
         <div className="bg-neutral-800 rounded-xl p-4">
@@ -13,7 +15,19 @@ const FollowBar: React.FC<FollowBarProps> = ({
                 Who to follow
             </h2>
             <div className="flex flex-col gap-6 mt-4">
-            {/*TODO: USER LIST*/}
+              {users.map((user: Record<string, any>) => (
+                <div className="flex flex-row gap-4" key={user.id}>
+                  <Avatar userId={user.id}/>
+                  <div className="flex flex-col ">
+                    <p className="text-white font-semibold text-sm">
+                      {user.name}
+                    </p>
+                    <p className="text-neutral-400 text-sm">
+                      @{user.username}
+                    </p>
+                  </div>
+                </div>
+              ))}
             </div>
         </div>
     </div>
