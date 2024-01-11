1a756f4d84fb46fbd43a8d8469e3fd8a8f80d69d
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-10 23:19:36 -0800

    Added EditModal to app component

--- twitter-clone/pages/_app.tsx
+++ twitter-clone/pages/_app.tsx
@@ -1,17 +1,19 @@
 import Layout from '@/components/Layout/Layout/Layout';
-import LoginModel from '@/components/Modals/LoginModel/LoginModel';
-import RegisterModel from '@/components/Modals/RegisterModel/RegisterModel';
+import LoginModal from '@/components/Modals/LoginModal/LoginModal';
+import RegisterModal from '@/components/Modals/RegisterModal/RegisterModal';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
+import EditModal from '@/components/Modals/EditModal/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 			<SessionProvider session={pageProps.session}>
 				<Toaster />
-				<RegisterModel />
-				<LoginModel />
+				<RegisterModal />
+				<LoginModal />
+				<EditModal />
 				<Layout>
 					<Component {...pageProps} />
 				</Layout>
