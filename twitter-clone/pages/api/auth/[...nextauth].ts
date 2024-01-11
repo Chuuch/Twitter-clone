aca2c6611674c7677c3b9a4acdea7c5973390de9
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-10 23:20:34 -0800

    added AuthOptions in nextauth

--- twitter-clone/pages/api/auth/[...nextauth].ts
+++ twitter-clone/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,18 @@
 import bcrypt from "bcrypt";
-import NextAuth from "next-auth/next";
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from '@/libs/prismadb';
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         CredentialsProvider({
             name: 'credentials',
             credentials: {
                 email: { label: 'email', type: 'text' },
-                password: { label: 'password', type: 'password' },
+                password: { label: 'password', type: 'password' } 
             },
             async authorize(credentials) {
                 if (!credentials?.email || !credentials?.password) {
@@ -29,22 +29,19 @@ export default NextAuth({
                     throw new Error('Invalid credentials');
                 }
 
-                const isValidPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
-
-                if (!isValidPassword) {
-                    throw new Error('Invalid credentials');
-                }
-
                 return user;
             }
         })
     ],
-    debug: process.env.NODE_ENV !== 'development',
+    debug: process.env.NODE_ENV === 'development',
     session: {
-        strategy: 'jwt'
+        strategy: 'jwt',
     },
     jwt: {
-        secret: process.env.NEXTAUTH_JWT_SECRET
+        secret: process.env.NEXTAUTH_JWT_SECRET,
     },
-    secret: process.env.NEXTAUTH_SECRET
-})
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
+
