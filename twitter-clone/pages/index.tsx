4e3ac3bf00db9e03eb4abad12cfa2cdfcc6d4527
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-10 23:19:49 -0800

    fixed label classname for Home

--- twitter-clone/pages/index.tsx
+++ twitter-clone/pages/index.tsx
@@ -1,14 +1,12 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header/Header'
-import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <Header label='home'/>
+    <Header label='Home'/>
     </>
   )
 }
