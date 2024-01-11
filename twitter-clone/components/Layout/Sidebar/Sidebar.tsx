7f0b36ee4786b262cf1f095af567d01d2903d364
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-10 18:00:06 -0800

    added signout to sidebar

--- twitter-clone/components/Layout/Sidebar/Sidebar.tsx
+++ twitter-clone/components/Layout/Sidebar/Sidebar.tsx
@@ -4,8 +4,11 @@ import SidebarLogo from '../SidebarLogo/SidebarLogo';
 import SidebarItem from '../SidebarItem/SidebarItem';
 import { BiLogOut } from 'react-icons/bi';
 import SidebarTweetButton from '../SidebarTweetButton/SidebarTweetButton';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { signOut } from 'next-auth/react';
 
 const Sidebar = () => {
+    const { data: currentUser } = useCurrentUser();
     const items = [
         {   
             label: 'Home',
@@ -36,7 +39,9 @@ const Sidebar = () => {
                      icon={item.icon}
                     />
                 ))}
-                <SidebarItem onClick={() => {}} icon={BiLogOut} label='Logout'/>
+                {currentUser && (
+                    <SidebarItem onClick={() => signOut()} icon={BiLogOut} label='Logout'/>
+                )}
                 <SidebarTweetButton />
             </div>
         </div>
