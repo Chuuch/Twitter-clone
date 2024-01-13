28af5e189d8650c8f00aec1b01953a9b0c4178a9
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-12 21:39:05 -0800

    added Tweet form to index component

--- twitter-clone/pages/index.tsx
+++ twitter-clone/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header/Header'
+import Form from '@/components/Form/Form'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
     <Header label='Home'/>
+    <Form />
     </>
   )
 }
