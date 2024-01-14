9e17251c5152599639501c46ea70300432d396a7
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-13 22:27:33 -0800

    Changed name to Xitter

--- twitter-clone/components/Modals/LoginModal/LoginModal.tsx
+++ twitter-clone/components/Modals/LoginModal/LoginModal.tsx
@@ -58,7 +58,7 @@ const LoginModal = () => {
     const footerContent = (
         <div className="text-neutral-400 text-center mt-4">
             <p>
-                First time using Twitter?{' '}
+                First time using Xitter?{' '}
                 <span
                 onClick={onToggle}
                 className='text-white cursor-pointer hover:underline'>
