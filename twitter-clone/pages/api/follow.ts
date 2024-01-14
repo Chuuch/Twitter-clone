9b338889d10a597a4f3559d48f04e9d1b35c0191
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-13 22:19:30 -0800

    follow api

--- twitter-clone/pages/api/follow.ts
+++ twitter-clone/pages/api/follow.ts
@@ -35,8 +35,29 @@ export default async function handler(
             updatedFollowingIds.push(userId);
         }
 
+        try {
+            await prisma.notification.create({
+                data: {
+                    body: 'Someone followed you!',
+                    userId,
+                },
+            });
+
+            await prisma.user.update({
+                where: {
+                    id: userId
+                },
+                data: {
+                    hasNotification: true,
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            
+        }
+
         if (req.method === 'DELETE') {
-            updatedFollowingIds = updatedFollowingIds.filter(followingId => followingId !== userId);
+            updatedFollowingIds = updatedFollowingIds.filter((followingId) => followingId !== userId);
         }
 
         const updatedUser = await prisma.user.update({
